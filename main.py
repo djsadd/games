21e84a5d52c2c9cf52a077488325851cc7d49fc3
Erasil <erasil.kazah222@gmail.com>
2022-12-23 22:40:26 +0600

    formate text

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 import requests
-santa_speed = 70 #Метры в секунду скорость
-santa_max_volume = 200 #киллограммы максимум которые влезут в сани
-santa_max_weight = 100 #объект в дм3 которые влезут в сани
+
+santa_speed = 70  # Метры в секунду скорость
+santa_max_volume = 200  # киллограммы максимум которые влезут в сани
+santa_max_weight = 100  # объект в дм3 которые влезут в сани
 
 
 def sort_stack_id(stack):
-    stack_id = sorted(list(map(lambda x: lst_children[x], stack)), key=lambda x: x['x']+x['y'])
+    stack_id = sorted(list(map(lambda x: lst_children[x], stack)), key=lambda x: x['x'] + x['y'])
     print(stack_id)
-    print(list(map(lambda x: x['y']+x['x'], stack_id)))
+    print(list(map(lambda x: x['y'] + x['x'], stack_id)))
     return stack_id
 
 
-current_weight = 0 #текущий векс
-current_volume = 0 #текущий вес
+current_weight = 0  # текущий вес
+current_volume = 0  # текущий вес
 token = 'e5ea37ef-112c-4055-82ac-ab6fa4affecb'
 
-good = [] #список где будут храниться успешно пройденные дома
+good = []  # список где будут храниться успешно пройденные дома
 
-dt = requests.get('https://datsanta.dats.team/json/map/faf7ef78-41b3-4a36-8423-688a61929c08.json') #запрос получаю АПИ
-lst_gifts = dt.json()['gifts'] #получаю объект гифтс (id, вес, объем)
-lst_children = dt.json()['children'] #получая объект состоящий из координатов домов детей
+dt = requests.get('https://datsanta.dats.team/json/map/faf7ef78-41b3-4a36-8423-688a61929c08.json')  # запрос получаю АПИ
+lst_gifts = dt.json()['gifts']  # получаю объект гифтс (id, вес, объем)
+lst_children = dt.json()['children']  # получая объект состоящий из координатов домов детей
 stack_id = []
 for row in range(len(lst_children)):
     print(lst_gifts[row], lst_children[row], f"my stats: volume: {current_volume}, weight: {current_weight}")
-    if (int(lst_gifts[row]['volume']) + current_volume > 100)\
-            or\
-            (int(lst_gifts[row]['weight']) + current_weight > 200): #если вес или объем будет превышен мы отправляем санту раздаваит подарки
-        santa_coords_x, santa_coords_y = (0, 0) #начинаем с нулевых координат
+    if (int(lst_gifts[row]['volume']) + current_volume > 100) \
+            or \
+            (int(lst_gifts[row][
+                     'weight']) + current_weight > 200):  # если вес или объем будет превышен мы отправляем санту раздаваит подарки
+        santa_coords_x, santa_coords_y = (0, 0)  # начинаем с нулевых координат
         for res in sort_stack_id(stack_id):
             pass
 
         print('STOP', current_volume, current_weight, stack_id)
-        current_weight = 0 #сбрасываем значения
-        current_volume = 0 # сбросываем значения
+        current_weight = 0  # сбрасываем значения
+        current_volume = 0  # сбросываем значения
         stack_id.clear()
     else:
         current_weight = current_weight + int(lst_gifts[row]['weight'])
