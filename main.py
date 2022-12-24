f0189ddb3c82369453b2ec5469b5a05380a26981
Erasil <erasil.kazah222@gmail.com>
2022-12-24 17:01:39 +0600

    delete useless lists and finalize algorithm

--- main.py
+++ main.py
@@ -22,8 +22,7 @@ def sort_stack_id(stack):
 
 current_weight = 0  # текущий вес
 current_volume = 0  # текущий вес
-
-good = []  # список где будут храниться успешно пройденные дома
+santa_coords_x, santa_coords_y = (0, 0)  # начинаем с нулевых координат
 
 dt = requests.get('https://datsanta.dats.team/json/map/faf7ef78-41b3-4a36-8423-688a61929c08.json')  # запрос получаю АПИ
 lst_gifts = dt.json()['gifts']  # получаю объект гифтс (id, вес, объем)
@@ -38,7 +37,6 @@ for row in range(len(lst_children)):
             (int(lst_gifts[row][
                      'weight']) + current_weight > 200):  # если вес или объем будет превышен мы отправляем санту раздаваит подарки
 
-        santa_coords_x, santa_coords_y = (0, 0)  # начинаем с нулевых координат
         sorted_stack = sort_stack_id(stack_id)
         for i, res in enumerate(sorted_stack[0]):
             while santa_coords_x != res['x'] and santa_coords_y != res['y']:
@@ -59,9 +57,11 @@ for row in range(len(lst_children)):
                 moves.append({'x': res['x'], 'y': res['y']})
         stackOfBags.append(list(map(lambda x: x+1, sorted_stack[1][::])))
         print('STOP', current_volume, current_weight, stack_id)
+        santa_coords_x,santa_coords_y = 0, 0
         current_weight = 0  # сбрасываем значения
         current_volume = 0  # сбросываем значения
         stack_id.clear()
+
         # Добавляем элемент который пропускаем из-за того что закончилось место
         current_weight = current_weight + int(lst_gifts[row]['weight'])
         current_volume = current_volume + int(lst_gifts[row]['volume'])
