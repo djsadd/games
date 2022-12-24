73d33aa3a8ed9c661df4987801c80b4dc699cfc2
Erasil <erasil.kazah222@gmail.com>
2022-12-24 17:19:17 +0600

    new function reducion code and readability

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ token = 'e5ea37ef-112c-4055-82ac-ab6fa4affecb'
 url_first_round = 'https://datsanta.dats.team/api/round'
 headers = {"X-API-KEY": token}
 
-
 santa_speed = 70  # Метры в секунду скорость
 santa_max_volume = 200  # киллограммы максимум которые влезут в сани
 santa_max_weight = 100  # объект в дм3 которые влезут в сани
@@ -15,8 +14,14 @@ MAP_ID = 'faf7ef78-41b3-4a36-8423-688a61929c08.json'
 
 
 def sort_stack_id(stack):
+    '''
+    sorted function
+    :param sort:
+    :return: list
+    '''
+
     stacks_id = sorted(list(map(lambda x: lst_children[x], stack)), key=lambda x: x['x'] + x['y'])
-    think = sorted(stack, key=lambda x: lst_children[x]['x']+lst_children[x]['y'])
+    think = sorted(stack, key=lambda x: lst_children[x]['x'] + lst_children[x]['y'])
     return stacks_id, think
 
 
@@ -29,6 +34,31 @@ lst_gifts = dt.json()['gifts']  # получаю объект гифтс (id, в
 lst_children = dt.json()['children']  # получая объект состоящий из координатов домов детей
 
 stack_id = []
+
+
+def delivery_gift(stack):
+    santa_coords_x, santa_coords_y = (0, 0)  # начинаем с нулевых координат
+    sorted_stack = sort_stack_id(stack_id)
+    for i, res in enumerate(sorted_stack[0]):
+        while santa_coords_x != res['x'] and santa_coords_y != res['y']:
+            if santa_coords_x + santa_speed <= res['x']:
+                santa_coords_x += santa_speed
+            elif santa_coords_x + santa_speed > res['x']:
+                santa_coords_x = santa_coords_x + (res['x'] - santa_coords_x)
+            else:
+                santa_coords_x -= santa_speed
+
+            if santa_coords_y + santa_speed <= res['y']:
+                santa_coords_y += santa_speed
+            elif santa_coords_y + santa_speed > res['y']:
+                santa_coords_y = santa_coords_y + (res['y'] - santa_coords_y)
+            else:
+                santa_coords_y -= santa_speed
+        else:
+            moves.append({'x': res['x'], 'y': res['y']})
+        return sorted_stack
+
+
 for row in range(len(lst_children)):
     print(lst_gifts[row], lst_children[row], f"my stats: volume: {current_volume}, weight: {current_weight}")
 
@@ -37,27 +67,10 @@ for row in range(len(lst_children)):
             (int(lst_gifts[row][
                      'weight']) + current_weight > 200):  # если вес или объем будет превышен мы отправляем санту раздаваит подарки
 
-        sorted_stack = sort_stack_id(stack_id)
-        for i, res in enumerate(sorted_stack[0]):
-            while santa_coords_x != res['x'] and santa_coords_y != res['y']:
-                if santa_coords_x + santa_speed <= res['x']:
-                    santa_coords_x += santa_speed
-                elif santa_coords_x + santa_speed > res['x']:
-                    santa_coords_x = santa_coords_x + (res['x']-santa_coords_x)
-                else:
-                    santa_coords_x -= santa_speed
-
-                if santa_coords_y + santa_speed <= res['y']:
-                    santa_coords_y += santa_speed
-                elif santa_coords_y + santa_speed > res['y']:
-                    santa_coords_y = santa_coords_y + (res['y']-santa_coords_y)
-                else:
-                    santa_coords_y -= santa_speed
-            else:
-                moves.append({'x': res['x'], 'y': res['y']})
-        stackOfBags.append(list(map(lambda x: x+1, sorted_stack[1][::])))
+        sorted_stack = delivery_gift(stack_id)
+        stackOfBags.append(list(map(lambda x: x + 1, sorted_stack[1][::])))
         print('STOP', current_volume, current_weight, stack_id)
-        santa_coords_x,santa_coords_y = 0, 0
+        santa_coords_x, santa_coords_y = 0, 0
         current_weight = 0  # сбрасываем значения
         current_volume = 0  # сбросываем значения
         stack_id.clear()
@@ -65,32 +78,15 @@ for row in range(len(lst_children)):
         # Добавляем элемент который пропускаем из-за того что закончилось место
         current_weight = current_weight + int(lst_gifts[row]['weight'])
         current_volume = current_volume + int(lst_gifts[row]['volume'])
-        stack_id.append(lst_gifts[row]['id']-1)
+        stack_id.append(lst_gifts[row]['id'] - 1)
     else:
         current_weight = current_weight + int(lst_gifts[row]['weight'])
         current_volume = current_volume + int(lst_gifts[row]['volume'])
-        stack_id.append(lst_gifts[row]['id']-1)
+        stack_id.append(lst_gifts[row]['id'] - 1)
 else:
     if stack_id:
-        sorted_stack = sort_stack_id(stack_id)
-        for i, res in enumerate(sorted_stack[0]):
-            while santa_coords_x != res['x'] and santa_coords_y != res['y']:
-                if santa_coords_x + santa_speed <= res['x']:
-                    santa_coords_x += santa_speed
-                elif santa_coords_x + santa_speed > res['x']:
-                    santa_coords_x = santa_coords_x + (res['x']-santa_coords_x)
-                else:
-                    santa_coords_x -= santa_speed
-
-                if santa_coords_y + santa_speed <= res['y']:
-                    santa_coords_y += santa_speed
-                elif santa_coords_y + santa_speed > res['y']:
-                    santa_coords_y = santa_coords_y + (res['y']-santa_coords_y)
-                else:
-                    santa_coords_y -= santa_speed
-            else:
-                moves.append({'x': res['x'], 'y': res['y']})
-        stackOfBags.append(list(map(lambda x: x+1, sorted_stack[1][::])))
+        sorted_stack = delivery_gift(stack_id)
+        stackOfBags.append(list(map(lambda x: x + 1, sorted_stack[1][::])))
 
 dt = requests.post(url_first_round, headers=headers)
 print(dt.json())
